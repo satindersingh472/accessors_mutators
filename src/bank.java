6ee96d8f51f7fcaf636b8535cacd3cb40cba49e2
satinder <satindersingh472@gmail.com>
2023-01-04 10:38:47 -0700

    finished_accessors_mutatora

--- src/bank.java
+++ src/bank.java
@@ -1,2 +1,16 @@
 public class bank {
+    private chequing_account account_one;
+    private chequing_account account_two;
+
+    public bank() {
+        account_one = new chequing_account("satinder", 5000);
+        account_two = new chequing_account("sam", 10000);
+    }
+
+    public static void main(String[] args){
+        bank scotiabank = new bank();
+        System.out.println(scotiabank.account_one.getBalance());
+        scotiabank.account_one.set_balance(200);
+        System.out.println(scotiabank.account_one.getBalance());
+    }
 }
--- src/chequing_account.java
+++ src/chequing_account.java
@@ -1,2 +1,16 @@
 public class chequing_account {
+    private String name;
+    private int balance;
+
+    public chequing_account(String input_name, int input_balance){
+        name = input_name;
+        balance = input_balance;
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+    public void set_balance(int new_balance){
+        balance = new_balance;
+    }
 }
